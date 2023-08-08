c68c5cc9435a3b7ccce8f8c05bc6acf44274e6b5
BuildTools <jasper.emick@gmail.com>
2023-08-08 18:53:26 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# RiceInvWeb
+# riceweb
+
+https://drive.google.com/file/d/1NUBQzsYI3ODx3oFL1--MjzrJdDJranl1/view?usp=drive_link
---
+++ leaderboard.js
@@ -0,0 +1,380 @@
+var test = 0;
+
+var lbData = [
+	{
+		player: "obbyota",
+		brawlhalla: {
+			ones_placing: {
+				place: "13th",
+				points: 15,
+			},
+			twos_placing: {
+				place: "7th",
+				points: 20,
+			},
+			character: {
+				img: "assets/asuri_splash.png",
+				name: "Asuri"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "cola",
+		brawlhalla: {
+			ones_placing: {
+				place: "5th",
+				points: 50,
+			},
+			twos_placing: {
+				place: "4th",
+				points: 36,
+			},
+			character: {
+				img: "assets/vraxx_splash.png",
+				name: "Lord Vraxx"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "sp1cybo1",
+		brawlhalla: {
+			ones_placing: {
+				place: "7th",
+				points: 40,
+			},
+			twos_placing: {
+				place: "7th",
+				points: 20,
+			},
+			character: {
+				img: "assets/ember_splash.png",
+				name: "Ember"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "sruits",
+		brawlhalla: {
+			ones_placing: {
+				place: "5th",
+				points: 50,
+			},
+			twos_placing: {
+				place: "7th",
+				points: 20,
+			},
+			character: {
+				img: "assets/caspian_splash.png",
+				name: "Caspian"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "BestPurpleMaster",
+		brawlhalla: {
+			ones_placing: {
+				place: "13th",
+				points: 15,
+			},
+			twos_placing: {
+				place: "5th",
+				points: 28,
+			},
+			character: {
+				img: "assets/sidra_splash.png",
+				name: "Sidra"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "VietPapi",
+		brawlhalla: {
+			ones_placing: {
+				place: "9th",
+				points: 25,
+			},
+			twos_placing: {
+				place: "4th",
+				points: 36,
+			},
+			character: {
+				img: "assets/tezca_splash.png",
+				name: "Tezca"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "SioPaoPao",
+		brawlhalla: {
+			ones_placing: {
+				place: "9th",
+				points: 25,
+			},
+			twos_placing: {
+				place: "7th",
+				points: 20,
+			},
+			character: {
+				img: "assets/kor_splash.png",
+				name: "Kor"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "Striking Panic",
+		brawlhalla: {
+			ones_placing: {
+				place: "9th",
+				points: 25,
+			},
+			twos_placing: {
+				place: "2nd",
+				img: "assets/silver_medal.png",
+				points: 60,
+			},
+			character: {
+				img: "assets/diana_splash.png",
+				name: "Diana"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "Cody Travis",
+		brawlhalla: {
+			ones_placing: {
+				place: "1st",
+				img: "assets/gold_medal.png",
+				points: 120,
+			},
+			twos_placing: {
+				place: "1st",
+				img: "assets/gold_medal.png",
+				points: 80,
+			},
+			character: {
+				img: "assets/isaiah_splash.png",
+				name: "Isaiah"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "FewRaccoon69443",
+		brawlhalla: {
+			ones_placing: {
+				place: "4th",
+				points: 60,
+			},
+			twos_placing: {
+				place: "3rd",
+				img: "assets/bronze_medal.png",
+				points: 45,
+			},
+			character: {
+				img: "assets/cassidy_splash.png",
+				name: "Cassidy"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "Melodii",
+		brawlhalla: {
+			ones_placing: {
+				place: "13th",
+				points: 15,
+			},
+			twos_placing: {
+				place: "1st",
+				img: "assets/gold_medal.png",
+				points: 80,
+			},
+			character: {
+				img: "assets/scarlet_splash.png",
+				name: "Scarlet"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "Mcgurdstream",
+		brawlhalla: {
+			ones_placing: {
+				place: "9th",
+				points: 25,
+			},
+			twos_placing: {
+				place: "5th",
+				points: 28,
+			},
+			character: {
+				img: "assets/thatch_splash.png",
+				name: "Thatch"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "YouKooKoo",
+		brawlhalla: {
+			ones_placing: {
+				place: "Abstain",
+				points: 10,
+			},
+			twos_placing: {
+				place: "Abstain",
+				points: 5,
+			},
+			character: {
+				img: "assets/NA.png",
+				name: "N/A"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "Rattle",
+		brawlhalla: {
+			ones_placing: {
+				place: "5th",
+				points: 50,
+			},
+			twos_placing: {
+				place: "2nd",
+				img: "assets/silver_medal.png",
+				points: 60,
+			},
+			character: {
+				img: "assets/ulgrim_splash.png",
+				name: "Ulgrim"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "Pimplord",
+		brawlhalla: {
+			ones_placing: {
+				place: "13th",
+				points: 15,
+			},
+			twos_placing: {
+				place: "3rd",
+				img: "assets/bronze_medal.png",
+				points: 45,
+			},
+			character: {
+				img: "assets/roland_splash.png",
+				name: "Sir Roland"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	},
+	{
+		player: "Rantstand",
+		brawlhalla: {
+			ones_placing: {
+				place: "3rd",
+				img: "assets/bronze_medal.png",
+				points: 80,
+			},
+			twos_placing: {
+				place: "5th",
+				points: 28,
+			},
+			character: {
+				img: "assets/nai_splash.png",
+				name: "Queen Nai"
+			}
+		},
+		league: "-",
+		valorant: "-",
+		amongus: "-",
+		rocketleague: "-",
+		cs: "-",
+		random: "-"
+	}
+];
