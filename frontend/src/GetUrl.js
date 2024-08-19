dd1d37b0881638e1b33098ec650e86ff29b1acb2
jasperwemick <jasper.emick@gmail.com>
2024-08-19 13:00:26 -0400

    front adjust

--- frontend/src/GetUrl.js
+++ frontend/src/GetUrl.js
@@ -1,12 +1,12 @@
 const GetUrl = () => {
     console.log(process.env.NODE_ENV)
-    
+
     let url;
     if (process.env.NODE_ENV === 'development') {
-        url = `http://18.206.108.149:4000`
+        url = `https://18.206.108.149:4000`
     }
     else if (process.env.NODE_ENV === 'production') {
-        url = `http://18.206.108.149:4000`
+        url = `https://18.206.108.149:4000`
     }
 
     return url;
