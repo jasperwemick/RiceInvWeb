36f1772490e10307e05f50558532083af5ce6f25
EC2 Default User <ec2-user@ip-172-31-40-209.us-east-2.compute.internal>
2026-09-12 06:24:41 +0000

    nginx fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(riceinvitational.org rice.riceinvitational.org)
+domains=(riceinvitational.org www.riceinvitational.org)
 rsa_key_size=4096
 data_path="./certbot"
 email="jasper.emick@gmail.com" # Adding a valid address is strongly recommended
